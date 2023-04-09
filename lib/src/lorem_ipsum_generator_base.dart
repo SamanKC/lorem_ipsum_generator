42a33891bc37b410883f9298f60d5c110598d93e
Saman KC <saman.kc777@gmail.com>
2023-04-09 17:02:37 +0545

    pub.dev commit(0.0.1)

--- lib/src/lorem_ipsum_generator_base.dart
+++ lib/src/lorem_ipsum_generator_base.dart
@@ -4,12 +4,12 @@ class LoremIpsumGenerator {
   // The default Lorem Ipsum text to use.
   static const String _loremIpsumText =
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-  /*
-    Generate Lorem Ipsum text.
-    `words` is the number of words to generate, or null if not specified.
-    `paragraphs` is the number of paragraphs to generate, or null if not specified.
-    `wordsPerParagraph` is the number of words per paragraph (when `paragraphs` is specified).
-    Returns the generated text.*/
+
+  //Generate Lorem Ipsum text.
+  //`words` is the number of words to generate, or null if not specified.
+  //`paragraphs` is the number of paragraphs to generate, or null if not specified.
+  //`wordsPerParagraph` is the number of words per paragraph (when `paragraphs` is specified).
+  //Returns the generated text.
   static String generate({
     int? words,
     int? paragraphs,
